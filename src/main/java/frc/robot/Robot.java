89df9ef5277c5a8404956cdd69b540f894049d6c
Kevin Pfeffer <kevin.lee.pfeffer@gmail.com>
2024-03-11 16:38:44 -0500

    .

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.RunLED;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -27,6 +28,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
+    CommandScheduler.getInstance().schedule(new RunLED(Constants.LED_Constants.kLEDport, Constants.LED_Constants.kEffectLength));
     m_robotContainer = new RobotContainer();
   }
 
@@ -39,10 +41,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
+    
     CommandScheduler.getInstance().run();
   }
 
--- src/main/java/frc/robot/commands/RunLED.java
+++ src/main/java/frc/robot/commands/RunLED.java
@@ -4,22 +4,58 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 
-public class RunLEDs extends Command {
+public class RunLED extends Command {
   /** Creates a new RunLEDs. */
-  public RunLEDs() {
+
+  private AddressableLED led;
+  private AddressableLEDBuffer ledBuffer;
+  private int effectIndex;
+  private int effectLength;
+  public RunLED(int port, int length) 
+  {
+
+    led = new AddressableLED(port);
+    ledBuffer = new AddressableLEDBuffer(length);
+    led.setLength(length);
+
+    led.setData(ledBuffer);
+
+
+    effectIndex = 0;
+    effectLength = Constants.LED_Constants.kEffectLength;
+
+    addRequirements();;
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+   
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    for(int i = 0; i<ledBuffer.getLength(); i++){
+      if( (i>=effectIndex && i<=(effectIndex+effectLength-1))  || i<=(effectIndex+effectLength-1)%ledBuffer.getLength()){
+       ledBuffer.setHSV(i,115, 30, 100 );
+      }
+     ledBuffer.setHSV(i, 115, 100, 75);
+    }
 
+    effectIndex = (effectIndex+1)%ledBuffer.getLength();
+  }
+
+  //for when we do different colors, make methods for each color and put into if-statement chain in execute()
+  public void green(){
+
+  }
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
